fcd924cae79546482d806f72443d0472f53ac37e
subhankar1994 <subhankar.iamu@gmail.com>
2019-01-04 23:47:58 +0530

    csv

--- app/Helper.php
+++ app/Helper.php
@@ -1,38 +1,82 @@
 <?php
-function langOption(){
- 		$data=Config::get('laravellocalization.supportedLocales');
- 		$lang = @\Session::get('language');
-	 		foreach ($data as $key => $value) {
-	 			$selected='';
-	 			if($key==$lang){
-	 				$selected='selected="selected"';
-	 			}
+	function langOption(){
+		$data=Config::get('laravellocalization.supportedLocales');
+		$lang = @\Session::get('language');
+			foreach ($data as $key => $value) {
+				$selected='';
+				if($key==$lang){
+					$selected='selected="selected"';
+				}
+				echo '<option value="'.$key.'" '.$selected.' >'.$value['name'].'</option>';
+			}
+	}
+	function langlinkslist(){
+		$data=Config::get('laravellocalization.supportedLocales');
+			foreach ($data as $key => $value) {
+				echo '<a href="">'.$value['name'].'</a>';
+			}
+	}
+	function countryOption($id=''){
+		$country = App\Countries::all();
+		foreach ($country as $key => $value) {
+			if(isset($id)){
+				if($value->id == $id){
+					$selected='selected="selected"';
+			}else{
+				$selected='';
+			}
+			}
+			echo '<option value="'.$value->id.'" '.$selected.' >'.$value->countries_name.'</option>';
+		}
+	}
+	function getStateID($string, $country_id){
+		$data = App\StatesTranslation::where('states_name', '=' , $string)->get()->first();
+		if(!empty($data)){
+			$dataID = $data->states_id;
+		}else{
+			$state = new App\States;
+			$state->states_name = $string;
+			$state->countries_id = $country_id;
+			$state->save();
+			$dataID = $state->id;
+		}
+		return $dataID;
+	}
+	function getCountryID($string){
+		$data = App\CountriesTranslation::where('countries_name', '=' , $string)->get()->first();
+		if(!empty($data)){
+			$dataID = $data->countries_id;
+		}else{
+			$country = new App\Countries;
+			$country->countries_name = $string;
+			$country->save();
+			$dataID = $country->id;
+		}
+		return $dataID;
+	}
+	function getRegionID($string){
+		$data = App\RegionsTranslation::where('regions_name', '=' , $string)->get()->first();
+		if(!empty($data)){
+			$dataID = $data->regions_id;
+		}else{
+			$region = new App\Regions;
+			$region->regions_name = $string;
+			$region->save();
+			$dataID = $region->id;
+		}
+		return $dataID;
+	}
 
-	 			echo '<option value="'.$key.'" '.$selected.' >'.$value['name'].'</option>';
-	 		}
- 	}
-
-
- 	function langlinkslist(){
- 		$data=Config::get('laravellocalization.supportedLocales');
-	 		foreach ($data as $key => $value) {
-	 			echo '<a href="">'.$value['name'].'</a>';
-	 		}
- 	}
-
- 	function countryOption($id=''){
- 		$country = App\Countries::all();
- 		foreach ($country as $key => $value) {
- 			if(isset($id)){
- 				if($value->id == $id){
- 					$selected='selected="selected"';
- 			    }else{
- 			    	$selected='';
- 			    }
- 			}
- 			echo '<option value="'.$value->id.'" '.$selected.' >'.$value->countries_name.'</option>';
- 		}
- 	}
-
- ?>
- 
+	function getSpeciesID($string){
+		$data = App\SpeciesTranslation::where('species_name', '=' , $string)->get()->first();
+		if(!empty($data)){
+			$dataID = $data->species_id;
+		}else{
+			$species = new App\Species;
+			$species->species_name = $string;
+			$species->save();
+			$dataID = $species->id;
+		}
+		return $dataID;
+	}
+?>
--- app/Http/Controllers/Admin/HotelsController.php
+++ app/Http/Controllers/Admin/HotelsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\admin;
-
+use App\Hotels;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,7 +10,7 @@ class HotelsController extends Controller
     
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware('auth:admin'); 
     }
 
     public function index()
@@ -30,9 +30,62 @@ class HotelsController extends Controller
             $row = 0;
             do { 
                 if($row > 1){
-                    echo '<pre>';
-                    print_r($getData);
-                    echo '</pre>';
+                    if(!empty(array_filter($getData))){
+                        $region_id = getRegionID($getData[0]);
+                        $country_id = getCountryID($getData[1]);
+                        $state_id = getStateID($getData[2], $country_id);
+                        $town = $getData[3];
+                        $hotel_name = $getData[4];
+                        $rating = $getData[5];
+                        if($getData[6] == 1){
+                           $contact_status = 'R';
+                        }else{
+                           $contact_status = 'D';
+                        }
+                        /*$contact_status = $getData[7];*/
+                        $image_code = $getData[8];
+                        $email_id = $getData[9];
+                        $nearest_airport = $getData[10];
+                        $distance_airport = $getData[11];
+                        $transfers_mode = $getData[12];
+                        $additional_information = $getData[13];
+                        $services_amenities = $getData[14];
+                        $hotel_desc = $getData[15];
+                        $enthusiast_services = $getData[16];
+                        $activity_season = $getData[17];
+                        $species = explode(",", $getData[18]);
+                        $species_arr = array();
+                        for ($i=0; $i < count($species) ; $i++) { 
+                             $speciesID = getSpeciesID($species[$i]);
+                             array_push($species_arr, $speciesID);
+
+                        }
+                        $hotel = new Hotels;
+                        //Insert
+                        $hotel->hotel_name = $hotel_name;
+                        $hotel->hotel_desc = $hotel_desc;
+                        $hotel->region_id = $region_id;
+                        $hotel->country_id = $country_id;
+                        $hotel->state_id = $state_id;
+                        $hotel->town = $town;
+                        $hotel->email_id = $email_id;
+                        $hotel->nearest_airport = $nearest_airport;
+                        $hotel->distance_airport = $distance_airport;
+                        $hotel->transfers_mode = $transfers_mode;
+                        $hotel->additional_information = $additional_information;
+                        $hotel->services_amenities = $services_amenities;
+                        $hotel->enthusiast_services = $enthusiast_services;
+                        $hotel->activity_season = $activity_season;
+                        $hotel->image_code = $image_code;
+                        $hotel->contact_status = $contact_status;
+                        $hotel->rating = $rating;
+                        $hotel->save();
+                       /* for ($i=0; $i < count($species_arr) ; $i++) { 
+                            
+                        }*/
+                        echo $hotel->id;
+
+                    }
                 }
                 $row++;
             } while (($getData = fgetcsv($file, 0, ","))!== FALSE);
