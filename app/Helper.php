6fa6c5ed89149d505e1a7160bc06e6c00122224f
OTAlaravel <sonjoy.bhadra@met-technolonies.com>
2019-01-09 21:34:46 +0530

    commit after merge

--- app/Helper.php
+++ app/Helper.php
@@ -136,14 +136,20 @@
         $HotelexperiencesRelation->save();
 	}
 
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> 0b51e0a5af45067ddbc667ec5da19b7ed16f2e43
 	function split_name($name) {
 		$name = trim($name);
 		$last_name = (strpos($name, ' ') === false) ? '' : preg_replace('#.*\s([\w-]*)$#', '$1', $name);
 		$first_name = trim( preg_replace('#'.$last_name.'#', '', $name ) );
 		return array($first_name, $last_name);
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0b51e0a5af45067ddbc667ec5da19b7ed16f2e43
 	}
 ?>
