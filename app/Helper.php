c8dd1b923c999822377affe7838f32d3ce0d2c96
subhankar1994 <subhankar.iamu@gmail.com>
2019-01-10 14:44:18 +0530

    hotel slug

--- app/Helper.php
+++ app/Helper.php
@@ -170,4 +170,27 @@
 		return array($first_name, $last_name);
 
 	}
+
+	function createSlug($str, $delimiter = '-'){
+    	$slug = strtolower(trim(preg_replace('/[\s-]+/', $delimiter, preg_replace('/[^A-Za-z0-9-]+/', $delimiter, preg_replace('/[&]/', 'and', preg_replace('/[\']/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $str))))), $delimiter));
+    	return $slug;
+	} 
+
+	function hotelSlugExists($slug){
+		$data = App\HotelsTranslation::where('hotels_slug', '=' , $slug)->get()->first();
+		if(!empty($data)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function emailExists($email){
+		$data = App\User::where('email', '=' , $email)->get()->first();
+		if(!empty($data)){
+			return true;
+		}else{
+			return false;
+		}
+	}
 ?>
--- app/Hotels.php
+++ app/Hotels.php
@@ -8,7 +8,7 @@ class Hotels extends Model
 {
     use \Dimsav\Translatable\Translatable;
 
-    public $translatedAttributes = ['locale', 'hotels_name','hotels_desc'];
+    public $translatedAttributes = ['locale', 'hotels_name', 'hotels_slug', 'hotels_desc'];
 
-    protected $fillable = ['locale', 'hotels_name','hotels_desc'];
+    protected $fillable = ['locale', 'hotels_name', 'hotels_slug', 'hotels_desc'];
 }
